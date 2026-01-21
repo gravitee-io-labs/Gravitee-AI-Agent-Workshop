b483afe40b82eeef9956dd3cc343b67c52680ec4
Dorian BLANC <dorian.blanc@graviteesource.com>
2026-01-21 18:02:13 +0100

    ACME Hotels rebranding

--- hotel-website/config.js
+++ hotel-website/config.js
@@ -3,7 +3,7 @@
 window.APP_CONFIG = {
     agentCardUrl: 'http://localhost:8082/bookings-agent/.well-known/agent-card.json',
     oidcUrl: 'http://localhost:8092/gravitee/oidc/.well-known/openid-configuration',
-    clientId: 'gravitee-hotels',
-    clientSecret: 'gravitee-hotels',
+    clientId: 'acme-hotels',
+    clientSecret: 'acme-hotels',
     redirectUri: 'http://localhost:8002/'
 };
